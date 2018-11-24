00311cd220afb0699e41abad658732852b57801a
Romarios <romantgt@gmail.com>
2018-11-24 22:34:13 +0200

    game

--- pig-game/js/app.js
+++ pig-game/js/app.js
@@ -9,18 +9,63 @@ GAME RULES:
 const gameItems = {
   scores: [0, 0],
   roundScore: 0,
-  activePlayers: 0,
-  dice: Math.floor(Math.random() * 6) + 1,
-};
-
-const rollDice = () => {
-  let random = Math.floor(Math.random() * 6) + 1;
-  document.querySelector(
-    '#current-' + gameItems.activePlayers,
-  ).textContent = random;
+  activePlayers: 0
 };
 
 const diceRoll = document.querySelector('.dice');
-diceRoll.style.display = 'none';
 const rollButton = document.querySelector('.btn-roll');
-rollButton.addEventListener('click', rollDice);
+const holdButton = document.querySelector('.btn-hold');
+const newGameButton = document.querySelector('.btn-new');
+
+const startNewGame = () => {
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  gameItems.scores = [0, 0];
+  gameItems.roundScore = 0;
+  gameItems.activePlayers = 0;
+  diceRoll.style.display = 'none';
+  document.querySelector('.player-0-panel').classList.add('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+};
+
+startNewGame();
+
+const zeroingHandler = () => {
+  document.getElementById('current-' + gameItems.activePlayers).textContent = 0;
+  gameItems.roundScore = 0;
+  diceRoll.style.display = 'none';
+  gameItems.activePlayers === 0 ? gameItems.activePlayers = 1 : gameItems.activePlayers = 0;
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+};
+
+const rollBtnHandler = () => {  
+  let random = Math.floor(Math.random() * 6) + 1;
+  diceRoll.style.display = 'block';
+  diceRoll.src = './img/dice-' + random + '.png';
+  if(random !== 1) {
+    gameItems.roundScore += random;
+    document.getElementById('current-' + gameItems.activePlayers).textContent = gameItems.roundScore;
+  } else {
+    zeroingHandler();
+  }
+};
+
+const holdBtnHandler = () => {    
+    let playerScore = gameItems.scores[gameItems.activePlayers] += gameItems.roundScore;
+    document.getElementById('score-' + gameItems.activePlayers).textContent = playerScore;
+    zeroingHandler();
+};
+
+const newGameBtnHandler = () => {
+  startNewGame();
+  
+};
+
+
+rollButton.addEventListener('click', rollBtnHandler);
+holdButton.addEventListener('click', holdBtnHandler);
+newGameButton.addEventListener('click', newGameBtnHandler);
+
