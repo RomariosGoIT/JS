50387d658e7a305a0d22d6cc83927d995c088c9c
RomariosGoIT <romantgt@gmail.com>
2018-12-13 16:51:39 +0200

    add modules

--- project_one/app.js
+++ project_one/app.js
@@ -1,5 +1,31 @@
-const BUDGET_CONTROLLER = (() => {})();
+const BUDGET_CONTROLLER = (() => {
+  // some code
+})();
 
-const UI_CONTROLLER = (() => {})();
+const UI_CONTROLLER = (() => {
+  // some code
+  return {
+    getInput: () => {
+      let type = document.querySelector('.add__type').value;
+      let description = document.querySelector('.add__description').value;
+      let value = document.querySelector('.add__value').value;
+      return { type, description, value };
+    },
+  };
+})();
 
-const APP_CONTROLLER = (() => {})();
+const APP_CONTROLLER = ((budget, ui) => {
+  clickHandler = () => {
+    const input = ui.getInput();
+    console.log(input);
+  };
+  document.querySelector('.add__btn').addEventListener('click', clickHandler);
+
+  document.addEventListener('keydown', event => {
+    if (event.keyCode === 13 || event.which === 13) {
+      clickHandler();
+    }
+  });
+
+  // some code
+})(BUDGET_CONTROLLER, UI_CONTROLLER);
